a15bf452e081f2cf686310ea8c495982d715b7c6
Domenica Vizcarra <domeviz3410@gmail.com>
2022-10-23 17:15:11 -0500

    Cambiar de Activity

---
+++ app/src/main/java/com/example/dosactivity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dosactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Metodo para el boton siguiente
+    public void Siguiente(View view){
+        Intent siguiente=new Intent(this,SegundoActivity.class);
+        startActivity(siguiente);
+    }
+}
